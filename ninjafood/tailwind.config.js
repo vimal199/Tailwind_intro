16284b94c0f0d4eaea46225499439f0b603383f8
vimal199 <vimalbhardwaj8@gmail.com>
2023-07-23 23:37:33 +0530

    Adding custom css in config file

--- ninjafood/tailwind.config.js
+++ ninjafood/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './public/**/*.{html,js}',
-  ],
+  content: ['./public/**/*.{html,js}',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
   },
   plugins: [],
 }
